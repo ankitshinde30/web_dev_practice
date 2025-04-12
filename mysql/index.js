798068bccc0a150d4fe132ded0b62f2cde82bedc
Ankitshinde30 <ankit.shinde2611@gmail.com>
2025-04-12 21:47:01 +0530

    added home.ejs

--- mysql/index.js
+++ mysql/index.js
@@ -1,5 +1,16 @@
 const { faker, fi } = require('@faker-js/faker');
 const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+
+let port = 3000;
+
+// Middleware to parse JSON bodies
+app.set ('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 
 // Create the connection to database
 const connection = mysql.createConnection({
@@ -9,28 +20,52 @@ const connection = mysql.createConnection({
   password: 'Shinde@123',
 });
 
-let q= "INSERT INTO users(id,username,email,password) VALUES(?,?,?,?)";
-let user = ["123","123_newuser","abc@yahoo.co.in","123456"];
-
-
-try{
-    connection.query(q,user, (err, results) => {
-        if (err) throw err;
-        console.log("Tables in the database:", results);
-    });
-}catch(err){
-    console.error("Error fetching tables:", err);
-}
-finally{
-    connection.end();
-}
-
+// Create a random user using faker.js
 let getRandomUser=() =>{
-  return {
-    id: faker.string.uuid(),
-    username: faker.internet.username(), // before version 9.1.0, use userName()
-    email: faker.internet.email(),
-    password: faker.internet.password(),  };
-}
+    return [
+      faker.string.uuid(),
+      faker.internet.username(), // before version 9.1.0, use userName()
+      faker.internet.email(),
+      faker.internet.password()
+    ];
+  }
+
+
+app.get("/",(req,res)=>{
+  let q = "select count(*)from users";
+  try{
+        connection.query(q, (err, results) => {
+            if (err) throw err;
+            let count = results[0]['count(*)'];
+            console.log("Total no of count:", results);
+            res.render("home.ejs",{count});
+        });
+    }catch(err){
+        console.error("Error fetching count:", err);
+        res.send("Error fetching count:", err);
+    }
+    finally{
+        connection.end();
+    }
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+}); 
+
+
+// try{
+//     connection.query(q,[user], (err, results) => {
+//         if (err) throw err;
+//         console.log("Tables in the database:", results);
+//     });
+// }catch(err){
+//     console.error("Error fetching tables:", err);
+// }
+// finally{
+//     connection.end();
+// }
+
 
 // console.log(getRandomUser());
