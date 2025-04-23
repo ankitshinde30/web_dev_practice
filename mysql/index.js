0a0dbd23cba76d140bb33610343509e3ae3e1c49
Ankitshinde30 <ankit.shinde2611@gmail.com>
2025-04-23 09:55:22 +0530

    added editpost & showusers

--- mysql/index.js
+++ mysql/index.js
@@ -1,9 +1,14 @@
-const { faker, fi } = require('@faker-js/faker');
+const { faker, el } = require('@faker-js/faker');
 const mysql = require('mysql2');
 const express = require('express');
 const app = express();
-const { v4: uuidv4 } = require('uuid');
+// Removed as 'uuidv4' is not used
 const path = require('path');
+const methodOverride = require("method-override");
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 let port = 3000;
 
@@ -21,34 +26,83 @@ const connection = mysql.createConnection({
 });
 
 // Create a random user using faker.js
-let getRandomUser=() =>{
-    return [
-      faker.string.uuid(),
-      faker.internet.username(), // before version 9.1.0, use userName()
-      faker.internet.email(),
-      faker.internet.password()
-    ];
+// Removed as 'getRandomUser' is not used
+
+//Home route
+app.get("/", (req, res) => {
+  let q = "select count(*) from users";
+  try {
+    connection.query(q, (err, results) => {
+      if (err) throw err;
+      let count = results[0]['count(*)'];
+      console.log("Total no of count:", results);
+      res.send(`Total count: ${count}`);
+    });
+  } catch (err) {
+    console.error("Error fetching count:", err);
+    res.send(`Error fetching count: ${err.message}`);
+  }
+});
+
+//show Route
+app.get("/users", (req, res) => {
+  let q = "select * from users";
+  try {
+    connection.query(q, (err, users) => {
+      if (err) throw err;
+      // console.log("Users:", users);
+      res.render("showusers.ejs", { users });
+    });
+  } catch (err) {
+    console.error("Error fetching users:", err);
+    res.send(`Error fetching users: ${err.message}`);
+  }
+});
+
+//Edit Route 
+app.get("/users/:id/edit", (req, res) => {
+  let {id}= req.params;
+  let q=`select * from users where id='${id}'`;
+  try{
+    connection.query(q, (err,result) => {
+        if (err) throw err;
+        console.log("Users:", result);
+        let users = result[0];
+        res.render("editpost.ejs", { users });
+    });
+  } catch (err) {
+    console.error("Error fetching count:", err);
+    res.send("Error fetching count:", err);
+  }
+});
+
+//Update Route
+
+app.patch("/users/:id", (req, res) => {
+  let {id}= req.params;
+  let {password:formpass,username:newusername} = req.body;
+  let q=`select * from users where id='${id}'`;
+  try{
+    connection.query(q, (err,result) => {
+        if (err) throw err;
+        let users = result[0];
+        if(formpass!= users.password){
+            res.send("Password is not correct");
+        }else{
+            let q2=`update users set username='${newusername}' where id='${id}'`;
+            connection.query(q2, (err,result) => {
+                if (err) throw err;
+                res.redirect("/users");
+            });
+        }
+    });
+  } catch (err) {
+    console.error("Error fetching count:", err);
+    res.send("Error fetching count:", err);
   }
 
 
-app.get("/",(req,res)=>{
-  let q = "select count(*)from users";
-  try{
-        connection.query(q, (err, results) => {
-            if (err) throw err;
-            let count = results[0]['count(*)'];
-            console.log("Total no of count:", results);
-            res.render("home.ejs",{count});
-        });
-    }catch(err){
-        console.error("Error fetching count:", err);
-        res.send("Error fetching count:", err);
-    }
-    finally{
-        connection.end();
-    }
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
